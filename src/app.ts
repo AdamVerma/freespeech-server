df524c5c16d28cae044b4786d16319c5da5cd983
Merkie <merkie@post.com>
2022-11-04 17:57:07 -0500

    mb fix

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,9 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(helmet());
+app.use(express.json({ limit: "200mb" }));
+app.use(express.urlencoded({ limit: "200mb", extended: true }));
 app.use(cors());
-app.use(express.json());
 
 app.get<{}, { message: string }>("/", (req, res) => {
   res.json({
@@ -21,11 +22,9 @@ app.get<{}, { message: string }>("/", (req, res) => {
   });
 });
 
-app.use(express.json({ limit: "50mb" }));
 app.use(middlewares.handleAuth);
 app.use("/api/v1", api);
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
-app.use(express.json({ limit: "50mb" }));
 
 export default app;
